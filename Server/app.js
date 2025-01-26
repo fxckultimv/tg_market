dc9befeebabff22e0fcd8e88eaf041123e321d44
Stepan Pavlovsky <Stepanpavlovsky@gmail.com>
2025-01-26 15:16:50 +0300

    5

--- Server/app.js
+++ Server/app.js
@@ -63,7 +63,7 @@ app.use((err, req, res, next) => {
     res.status(500).json({ error: 'Something went wrong!' })
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
     logger.info(`Server is running on port ${PORT}`)
