8fd4485edde2d0c057a4743acbc5dddf81fa2aa2
Stepan Pavlovsky <Stepanpavlovsky@gmail.com>
2025-02-08 19:41:52 +0300

    verificate channel

--- Server/controllers/channelsController.js
+++ Server/controllers/channelsController.js
@@ -7,15 +7,11 @@ class channelController {
 
         try {
             const result = await db.query(
-                `SELECT DISTINCT vc.channel_id, vc.channel_name, vc.channel_url, vc.subscribers_count, vc.views, vc.channel_tg_id,
-                CASE 
-                WHEN p.channel_id IS NOT NULL THEN TRUE
-                ELSE FALSE
-                END AS has_product
+                `SELECT DISTINCT vc.channel_id, vc.channel_name, vc.channel_url, vc.subscribers_count, vc.views, vc.channel_tg_id
                 FROM verifiedchannels AS vc
-                JOIN products p ON p.channel_id = vc.channel_id
                 WHERE vc.user_id = $1
-                ORDER BY vc.subscribers_count DESC`,
+                ORDER BY vc.subscribers_count DESC;
+                `,
                 [user_id]
             )
 
